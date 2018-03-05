0ceeba661516c6573328f3618079e218ac30e71b
Nicholas Baron <nick.john.baron@gmail.com>
2018-03-04 17:37:21 -0700

    FInal working update

--- client.py
+++ client.py
@@ -1,8 +1,11 @@
 from encrypt import encrypt
 from decrypt import decrypt
 from helpers import read_file
+from helpers import get_keys
 import sys
 import socket
+
+print("Welcome to the UDP ECHO client!")
 
 if len(sys.argv) != 5:
     print("[ERROR] " + str(sys.argv[0]) + " Incorrect command line arguments")
@@ -10,23 +13,22 @@ if len(sys.argv) != 5:
 
 udp_ip = sys.argv[1]
 udp_port = int(sys.argv[2])
-keys = bytearray(sys.argv[3], "utf-8")
+keys = get_keys(sys.argv[3])
 message = encrypt(keys, read_file(sys.argv[4]))
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+server_address = (udp_ip, udp_port)
 sock.settimeout(1)
 
 try:
-    print("[LOG] Sending infromation.")
-    sock.sendto(message, (udp_ip, udp_port))
+    print("[LOG] Sending information to", server_address[0], server_address[1])
+    sock.sendto(message, server_address)
     while True:
         print("[LOG] Waiting for response.")
-        try:
-            data, address = sock.recvfrom(4096)
-        except:
-            print("[WARNING] Receiver Timed Out.")
-            exit(0)
-        print("[LOG] Response received.")
+        data, address = sock.recvfrom(4096)
+        print("[LOG] Response received from", address[0], address[1])
         print(str(decrypt(keys, data))[12:-2])
+except:
+    print("[WARNING] Receiver Timed Out.")
 finally:
     sock.close()
--- server.py
+++ server.py
@@ -5,12 +5,14 @@ from decrypt import decrypt
 from collections import deque
 import datetime
 
+print("Welcome to the UDP ECHO server!")
+
 if len(sys.argv) != 2:
     print("[ERROR] " + str(sys.argv[0]) + " Incorrect command line arguments.")
     exit(1)
 
 que = deque(maxlen=5)
-key_dic = {"127.0.0.1":"aeiouaei","10.10.4.1":"abcdefgh","10.10.2.2":"bcdefghi","10.10.3.2":"cdefghij","10.10.4.2":"defghijk","10.10.5.2":"efghijkl","10.10.6.2":"fghijklm"}
+key_dic = {"127.0.0.1":"aeiouaei","10.10.4.1":"abcdefgh","10.10.6.1":"bcdefghi","10.10.5.1":"cdefghij","10.10.3.1":"defghijk","10.10.2.1":"fghijklm"}
 
 udp_port = int(sys.argv[1])
 
@@ -21,15 +23,18 @@ try:
     while True:
         print("[LOG] Waiting to receive.")
         data, address = sock.recvfrom(4096)
-        print("[LOG] Message Received.")
-        quedata = bytearray(datetime.datetime.now().strftime("%H:%M:%S.%f") + " " + str(address[0]) + " ", "utf-8")
-        key = bytes(key_dic[str(address[0])], "utf-8")
-        quedata += decrypt(key, data)
-        print(str(quedata)[12:-2])
-        que.append(encrypt(key, quedata))
-        print("[LOG] Replying:")
-        for i in range(0, len(que)):
-            sock.sendto(que[i], address)
-            print(str(que[i])[12:-2])
+        print("[LOG] Message Received from", address[0], address[1])
+        try:
+            quedata = bytearray(datetime.datetime.now().strftime("%H:%M:%S.%f") + " " + str(address[0]) + " ", "utf-8")
+            key = bytes(key_dic[str(address[0])], "utf-8")
+            quedata += decrypt(key, data)
+            print(str(quedata)[12:-2])
+            que.append(quedata)
+            print("[LOG] Replying to", address[0], address[1])
+            for i in range(0, len(que)):
+                sock.sendto(encrypt(key, que[i]), address)
+                print(str(que[i])[12:-2])
+        except:
+            print("[ERROR] Received information from", address[0], address[1], "with no key!")
 finally:
     sock.close()
